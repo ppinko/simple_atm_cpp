432dceeafa9436daed758d320a849ae01661abba
ppinko <p.pinkowicz@gmail.com>
2020-04-05 22:10:33 +0200

    Adding showBalance function

--- simple_atm.cpp
+++ simple_atm.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <map>
 
+// global parameters
+int balance = 0;
+
+// all function prototypes
+void showBalance();
+
+void showBalance() {
+    std::cout << "You have: $" << balance << std::endl;
+}
+
 int main(){
     // creating a database including initial account numbers and PIN
     std::map<int, int> *db_account_pin = new std::map<int, int>; 
     for (int i {10000}; i <= 99999; i++){
         db_account_pin->emplace(i, i);
     }
-
+    showBalance();
     delete db_account_pin;
     return 0;
 }
