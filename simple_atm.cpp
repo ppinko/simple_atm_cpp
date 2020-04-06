3327b86fcd9b5089be71b76c5e60e1ff8167be70
ppinko <p.pinkowicz@gmail.com>
2020-04-06 10:26:26 +0200

    Adding function isValidAccount and test for it

--- simple_atm.cpp
+++ simple_atm.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <map>
+#include <algorithm>
+#include <iterator>
+#include <iomanip>
 
 // global parameters
 int balance = 0;
@@ -7,19 +10,23 @@ int balance = 0;
 // all function prototypes
 void showBalance();
 bool isGoodPin(int pin, int account_no, std::map<int, int> *db);
+bool isValidAccount(int account_no, std::map<int, int> *db);
 int getUserOption();
 
 void showBalance() {
     std::cout << "You have: $" << balance << std::endl;
 }
 
-
 bool isGoodPin(int pin, int account_no, std::map<int, int> *db){
     return (pin == db->at(account_no));
 }
 
+bool isValidAccount(int account_no, std::map<int, int> *db){
+    return (db->find(account_no) != db->end());
+}
+
 int getUserOption(){
-	int selectedOption {};
+	int choosenOption {};
 
 	std::cout << "Main menu:" << std::endl;
 	std::cout << " 1 - Show my balance" << std::endl;
@@ -27,17 +34,24 @@ int getUserOption(){
 	std::cout << " 3 - Deposit funds" << std::endl;
 	std::cout << " 4 - Exit" << std::endl;
 	std::cout << "Please enter a choice: " << std::endl;
-	std::cin >> selectedOption;
+	std::cin >> choosenOption;
 	
-	return selectedOption;	
+	return choosenOption;	
 }
 
 int main(){
     // creating a database including initial account numbers and PIN
+    
     std::map<int, int> *db_account_pin = new std::map<int, int>; 
     for (int i {10000}; i <= 99999; i++){
         db_account_pin->emplace(i, i);
     }
+    
+    // Test isValidAccount
+    std::cout << std::boolalpha;
+    std::cout << isValidAccount(12345, db_account_pin) << " should be true" << std::endl;
+    std::cout << isValidAccount(1234, db_account_pin) << " should be false" << std::endl;
+
     showBalance();
     delete db_account_pin;
     return 0;
